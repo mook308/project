8ab9b684071d584cc955bc974bd4986520570c6f
Ktnnn3 <59277393+Ktnnn3@users.noreply.github.com>
2020-03-18 16:07:09 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 //Project Compro
-#include"main_program.h"
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<fstream>
+#include<vector>
+#include<cctype>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+int mymoney=0,gameover=0;
+int gamestart=0,howtoplay=0,leaderB=0;
+struct Card{
+	string deck;
+	string namecard;
+	int score;
+};
+int betmoney (int);
+
+void Addmoney(int,int &);
+
+void removemoney(int,int &);
+
+void checkmoney ();
+
+void checkgamemode(int);
 
 int main(){
 	int menu=0;	
@@ -24,13 +48,14 @@ int main(){
 				char desk[100];
 				char name[100];
 				sscanf(ccard.c_str(),"%[^;];%[^;];%d",desk,name,&c.score);
-				c.desk = desk;
+				c.deck = desk;
 				c.namecard = name;
 				cc.push_back(c);
 				//cout<< c.score << "\n";
 			}
 			int i=rand()%52;
-			cout<<ccard[i].score;
+			cout<<cc[i].score<<endl;
+			break;
 			
 			
 			
@@ -86,4 +111,52 @@ int main(){
 	
 	
 	
+}
+int betmoney (int x){ //x cin from player
+	if(x>mymoney) return 0;
+	else return x;
+	
+}
+
+
+
+void Addmoney(int x,int &y){ //x from function bet.,y = mymoney
+	y += x*2;
+}
+
+
+
+
+void removemoney(int x,int &y){ //x from function bet.,y = mymoney
+	y -= x;
+}
+
+
+
+
+void checkmoney (){
+	if(mymoney<=0) gameover=1;
+}
+
+
+
+
+void checkgamemode(int x){
+	if(x==1) gamestart=1;
+	else if(x==2) howtoplay=1;
+	else if(x==3) leaderB=1;
+	
+}
+
+
+
+
+int Hit(vector<Card> &a,vector<string> &b){
+	int i = rand()%52;
+	//cout<< i <<"\n";
+	string x = a[i].deck ;
+	string y = a[i].namecard;
+	int z = a[i].score;
+	b.push_back(x+y);
+	return a[i].score;	
 }
