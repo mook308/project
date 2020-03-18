23339b861bb7ab0b5d2efd8fd5f0b070ef132ef5
Ktnnn3 <59277393+Ktnnn3@users.noreply.github.com>
2020-03-18 15:19:07 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,70 @@
+//Project Compro
+#include"main_program.h"
+
+
+int main(){
+	int menu=0;
+	while (menu==0){
+		cout<<"Type 1 to start game"<<endl;
+		cout<<"Type 2 how to play the game"<<endl;
+		cout<<"Type 3 check leader board"<<endl;
+		int input_menu;
+		cin>>input_menu;
+		checkgamemode(input_menu);
+		while(gamestart==1){
+			cout<<"EI";
+			
+			
+			
+		}
+		
+		
+		while(howtoplay==1){
+			cout<<"ihi";
+			
+			
+			
+			
+		}
+		
+		while(leaderB==1){
+		cout<<"iejiwofjp";	
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
